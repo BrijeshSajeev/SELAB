879e11dd30e1814b2a31b23afd1a0d51348c356b
BrijeshSajeev <brijesh00300@gmail.com>
2024-05-07 05:20:59 +0530

    **

--- CPP/Cause_of_failuer/mat_mul.cpp
+++ CPP/Cause_of_failuer/mat_mul.cpp
@@ -17,22 +17,22 @@ void printMatrix(int rows, int cols, int mat[10][10]) {
 
 int main(){
     int m1[10][10],m2[10][10],m3[10][10];
-    int r1,r2,c1,c2;
+    int row1,row2,col1,col2;
 
     printf("\nEnter no of Rows and columns in Matrix 1 > ");
-    scanf("%d%d",&r1,&c1);
+    scanf("%d%d",&row1,&col1);
     printf("\nEnter no of Rows and columns in Matrix 2 > ");
-    scanf("%d%d",&r2,&c2);
+    scanf("%d%d",&row2,&col2);
 
-    if(c1 !=r2){
+    if(col1 !=row2){
         printf("\nCase 1:");
         printf("\nError: No of Column in matrix 1 should equal to number rows in matrix 2:");
         return 0;
     }
     printf("\nEnter the element of M1\n");
 
-    for(int i=0;i<r1;i++){
-        for(int j=0;j<c1;j++){
+    for(int i=0;i<row1;i++){
+        for(int j=0;j<col1;j++){
             if(scanf("%d",&m1[i][j])!=1){
                 printf("Matrix multiplication not possible cause of not integer value");
                 return 0;
@@ -40,10 +40,10 @@ int main(){
         }
     }
     printf("\nMatrix 1\n");
-    printMatrix(r1,c1,m1);
+    printMatrix(row1,col1,m1);
      printf("\nEnter the element of M2\n");
-    for(int i=0;i<r2;i++){
-        for(int j=0;j<c2;j++){
+    for(int i=0;i<row2;i++){
+        for(int j=0;j<col2;j++){
             if(scanf("%d",&m2[i][j])!=1){
                 printf("Matrix multiplication not possible cause of not integer value");
                 return 0;
@@ -51,15 +51,16 @@ int main(){
         }
     }
     printf("\nMatrix 2\n");
-    printMatrix(r2,c2,m2);
-    for(int i=0;i<r1;i++){
-        for(int j=0;j<c2;j++){
+    printMatrix(row2,col2,m2);
+    
+    for(int i=0;i<row1;i++){
+        for(int j=0;j<col2;j++){
             m3[i][j] =0;
-            for(int k=0;k<c1;k++){
-                m3[i][j] =m3[i][j]+ m1[i][j] * m2[k][j];
+            for(int k=0;k<col1;k++){
+                m3[i][j] =m3[i][j]+ m1[i][k] * m2[k][j];
             }
         }
     }
     printf("\nResultant Matrix\n");
-    printMatrix(r1,c2,m3);
+    printMatrix(row1,col2,m3);
 }
